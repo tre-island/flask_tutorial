d44ed6fd53d17f28e95f4cef0229b6c982eb997c
Tre Island <t.island84@gmail.com>
2020-06-07 14:46:48 -0600

    initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+import uuid
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=str(uuid.uuid4()),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_pyfile('config.py', silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
